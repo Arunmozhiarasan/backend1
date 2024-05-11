f23fa4acbdd16d65245503e7e4e4bd5c31baef2e
krish4201 <rkkramesh2001@gmail.com>
2024-05-11 20:25:25 +0530

    backend

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const db =require("./Mongo/db")
+const db = require("./Mongo/db")
+const express = require("express")
+const app=express()
+const cors =require("cors");
+
+app.listen(8000,()=>{
+    console.log(`server runs in ${process.env.PORT}`)
+})
+app.use(cors())
+
+app.use(express.json())
+
+app.get("/",cors(),(req,res)=>{
+
+})
+
+app.post("/",(req,res)=>{
+    res.send({"msg":"hello"});
+})
